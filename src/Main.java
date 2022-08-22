de3e23137991a0b24e8b0e4adef6606f305e256d
Kot-Zmot <meledin.av52@gmail.com>
2022-08-22 21:09:45 +0300

    code is done

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketcost = 5000;
+        int bonuscost = 20;
+        int totalbonus = ticketcost / bonuscost;
+
+        System.out.println("Всего начислено " + totalbonus + " бонусных миль(и)");
+
+    }
+}
